21ffaaf49222dbbd35e2f09633ff560e53932416
reisemh3 <reisemh3@gmail.com>
2021-12-06 21:52:00 +0100

    Add gestiion and admin

---
+++ Admin.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Annuaire // Note: actual namespace depends on the project name.
+{
+    public class Admin //Formulaire Administrateur
+    {
+
+    }
+}
